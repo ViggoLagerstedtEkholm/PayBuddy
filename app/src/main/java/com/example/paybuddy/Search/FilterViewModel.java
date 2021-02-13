dd12e9289e26aa17adcf692eff0e5ede1a728d57
ViggoLagerstedtEkholm <viggo.lagerstedtekholm@gmail.com>
2021-02-13 04:49:26 +0100

    Removed and created ViewModel for searching

--- app/src/main/java/com/example/paybuddy/Search/FilterViewModel.java
+++ app/src/main/java/com/example/paybuddy/Search/FilterViewModel.java
@@ -1,10 +1,10 @@
-package com.example.paybuddy.History.ViewModel;
+package com.example.paybuddy.Search;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class FilterViewModelHistory extends ViewModel {
+public class FilterViewModel extends ViewModel {
     private final MutableLiveData<String> selected = new MutableLiveData<String>();
 
     public void select(String item) {
