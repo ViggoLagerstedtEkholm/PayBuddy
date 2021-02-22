1f95e93f71831f82ad1dcd775864a2ea71de24c2
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-02-22 23:56:21 +0100

    Added location model.

--- app/src/main/java/com/example/paybuddy/Models/LocationModel.java
+++ app/src/main/java/com/example/paybuddy/Models/LocationModel.java
@@ -2,15 +2,31 @@ package com.example.paybuddy.Models;
 
 import android.widget.TextView;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.example.paybuddy.R;
 
-//TODO
+@Entity(tableName="Location_table")
 public class LocationModel {
+    @PrimaryKey(autoGenerate = true)
+    private int ID;
+
     private double latitude;
     private double longitude;
     private double altititude;
     private double accuracy;
     private String adress;
+
+    public long getOccasionID() {
+        return occasionID;
+    }
+
+    public void setOccasionID(long occasionID) {
+        this.occasionID = occasionID;
+    }
+
+    private long occasionID;
 
     public LocationModel(double latitude, double longitude, double altititude, double accuracy, String adress) {
         this.latitude = latitude;
@@ -18,6 +34,14 @@ public class LocationModel {
         this.altititude = altititude;
         this.accuracy = accuracy;
         this.adress = adress;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
     }
 
     public double getLatitude() {
