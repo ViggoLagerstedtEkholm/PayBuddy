536fb0e0311a734bc84cb35b92b23d46582afaed
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-02-22 23:49:55 +0100

    Added new relation.

--- app/src/main/java/com/example/paybuddy/Models/OccasionWithItems.java
+++ app/src/main/java/com/example/paybuddy/Models/OccasionWithItems.java
@@ -15,4 +15,9 @@ public class OccasionWithItems {
     )
     public List<ItemModel> itemModelList;
 
+    @Relation(
+            parentColumn = "ID",
+            entityColumn = "occasionID"
+    )
+    public LocationModel locationModel;
 }
