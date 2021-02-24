a2dcc44059f4ef5d05bce989f8d62d51c6de7e69
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-02-24 04:51:04 +0100

    Added enum for deletion types.

--- app/src/main/java/com/example/paybuddy/Repositories/Repository.java
+++ app/src/main/java/com/example/paybuddy/Repositories/Repository.java
@@ -1,12 +1,18 @@
-package com.example.paybuddy.MVVM.Repositories;
+package com.example.paybuddy.Repositories;
 
 import androidx.lifecycle.LiveData;
-
-import com.example.paybuddy.Models.OccasionWithItems;
 
 import java.util.List;
 
 public abstract class Repository<T> {
+
+    public enum DELETE_TYPE{
+        DELETE_ALL_EXPIRED,
+        DELETE_ALL_HISTORY,
+        DELETE_ALL_UNPAID,
+        DELETE_ALL
+    }
+
     abstract void insert(T... entity);
     abstract void insert(List<T> entities);
 
@@ -14,7 +20,7 @@ public abstract class Repository<T> {
 
     abstract void delete(T entity);
     abstract void delete(List<T> entity);
-    abstract void deleteAll();
+    abstract void deleteAll(DELETE_TYPE delete_type);
 
     abstract LiveData<List<T>> getAll();
 }
