c374b7932396218e846d698940c23ae4a319159a
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-02-27 03:26:54 +0100

    Added Contacts.

---
+++ app/src/main/java/com/example/paybuddy/Models/Contact.java
@@ -0,0 +1,31 @@
+package com.example.paybuddy.Models;
+
+/**
+ * This class contains all the data our contact should have.
+ * We also use getters and setters to access this data.
+ */
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
