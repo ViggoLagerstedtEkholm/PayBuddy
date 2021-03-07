4f21067a93b9d0f3624baca604b6ce3b2c6ffd76
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-03-07 02:59:55 +0100

    Added test data.

--- app/src/main/java/com/example/paybuddy/Database/DatabaseHelper.java
+++ app/src/main/java/com/example/paybuddy/Database/DatabaseHelper.java
@@ -27,7 +27,7 @@ public abstract class DatabaseHelper extends RoomDatabase {
 
    public static synchronized DatabaseHelper getInstance(Context context){
       if(instance == null){
-         instance = Room.databaseBuilder(context.getApplicationContext(), DatabaseHelper.class, "myRoomDBB")
+         instance = Room.databaseBuilder(context.getApplicationContext(), DatabaseHelper.class, "PayBuddyDB")
                  .fallbackToDestructiveMigration()
                  .addCallback(roomCallBack)
                  .build();
@@ -54,28 +54,106 @@ public abstract class DatabaseHelper extends RoomDatabase {
       }
       @Override
       protected Void doInBackground(Void... voids) {
-         long id = occasionDao.insert(new OccasionModel("2021", "TestData1", false, true));
-         ItemModel item = new ItemModel(20.5, "TestDataItem", 2, "Adam");
-         LocationModel locationModel = new LocationModel(1.1,1.1,5.2, 100.0, "Test");
-         item.setOccasionID(id);
-         locationModel.setOccasionID(id);
-         locationDAO.insert(locationModel);
-         itemsDAO.insert(item);
-         itemsDAO.insert(item);
+         //Pending occasion test data.
+         long id = occasionDao.insert(new OccasionModel("Mar 5, 2021", "Lunch in the city", false, false));
+         ItemModel itemInOccasion1 = new ItemModel(20.5, "Coca Cola", 2, "Adam");
+         ItemModel item2InOccasion1 = new ItemModel(18.5, "Sprite", 2, "Jakob");
+         ItemModel item3InOccasion1 = new ItemModel(60, "Day's special", 1, "Adam");
+         ItemModel item4InOccasion1 = new ItemModel(75, "Hamburger with fries", 1, "Jakob");
 
-         long id2 = occasionDao.insert(new OccasionModel("2021", "TestData2", false, false));
-         ItemModel item2 = new ItemModel(250.5, "TestDataItem2", 3, "David");
-         LocationModel locationModel2 = new LocationModel(1.6,2.1,7.2, 50.0, "Test2");
-         item2.setOccasionID(id2);
-         locationModel.setOccasionID(id2);
-         locationDAO.insert(locationModel);
-         itemsDAO.insert(item2);
+         LocationModel locationModelInOccasion1 = new LocationModel(59.309059,18.103723,0.0, 100.0, "Jan Inghes Torg 22, 120 71 Stockholm");
 
-         occasionDao.insert(new OccasionModel("2021", "TestData3", false, false));
-         occasionDao.insert(new OccasionModel("2021", "TestData2", false, true));
-         occasionDao.insert(new OccasionModel("2021", "TestData3", false, true));
-         occasionDao.insert(new OccasionModel("2021", "TestData3", true, false));
+         itemInOccasion1.setOccasionID(id);
+         item2InOccasion1.setOccasionID(id);
+         item3InOccasion1.setOccasionID(id);
+         item4InOccasion1.setOccasionID(id);
 
+         locationModelInOccasion1.setOccasionID(id);
+
+         locationDAO.insert(locationModelInOccasion1);
+
+         itemsDAO.insert(itemInOccasion1);
+         itemsDAO.insert(item2InOccasion1);
+         itemsDAO.insert(item3InOccasion1);
+         itemsDAO.insert(item4InOccasion1);
+
+         //Pending occasion test data 2.
+         long id_2 = occasionDao.insert(new OccasionModel("Mar 5, 2021", "Mc Donald's", false, false));
+         ItemModel itemInOccasion1_2 = new ItemModel(12.5, "Cheese Burger", 2, "Adam");
+         ItemModel item2InOccasion1_2 = new ItemModel(60, "Big Mac", 1, "Jakob");
+
+         LocationModel locationModelInOccasion1_2 = new LocationModel(59.339006,18.090841,0.0, 100.0, "Karlaplan 13, 115 20 Stockholm");
+
+         itemInOccasion1_2.setOccasionID(id_2);
+         item2InOccasion1_2.setOccasionID(id_2);
+
+         locationModelInOccasion1_2.setOccasionID(id_2);
+
+         locationDAO.insert(locationModelInOccasion1_2);
+
+         itemsDAO.insert(itemInOccasion1_2);
+         itemsDAO.insert(item2InOccasion1_2);
+
+
+         //Paid occasion test data.
+         long id2 = occasionDao.insert(new OccasionModel("Mar 6, 2021", "Tasty hamburgers", true, false));
+         ItemModel itemInOccasion2 = new ItemModel(55, "Cheese 'n' Bacon", 3, "David");
+         ItemModel item2InOccasion2 = new ItemModel(67, "Triple Cheese", 3, "Jakob");
+         ItemModel item3InOccasion2 = new ItemModel(70, "Pepper Jack Jr", 3, "Adam");
+
+         LocationModel locationModelInOccasion2 = new LocationModel(59.314861,18.073395,0.0, 100.0, "Götgatan 47B, 116 21 Stockholm");
+
+         itemInOccasion2.setOccasionID(id2);
+         item2InOccasion2.setOccasionID(id2);
+         item3InOccasion2.setOccasionID(id2);
+
+         locationModelInOccasion2.setOccasionID(id2);
+
+         locationDAO.insert(locationModelInOccasion2);
+
+         itemsDAO.insert(itemInOccasion2);
+         itemsDAO.insert(item2InOccasion2);
+         itemsDAO.insert(item3InOccasion2);
+
+         //Expired occasion test data.
+         long id3 = occasionDao.insert(new OccasionModel("Mar 6, 2021", "Quality ribs", false, true));
+         ItemModel itemInOccasion3 = new ItemModel(100, "10 Ribs", 1, "Robert");
+         ItemModel item2InOccasion3 = new ItemModel(119, "Beyond Burger", 1, "Simon");
+         ItemModel item3InOccasion3 = new ItemModel(99, "Classic Burger", 1, "Justin");
+
+         LocationModel locationModelInOccasion3 = new LocationModel(59.332905,18.043541,0.0, 100.0, "Fleminggatan 27, 112 26 Stockholm");
+
+         itemInOccasion3.setOccasionID(id3);
+         item2InOccasion3.setOccasionID(id3);
+         item3InOccasion3.setOccasionID(id3);
+
+         locationModelInOccasion3.setOccasionID(id3);
+
+         locationDAO.insert(locationModelInOccasion3);
+
+         itemsDAO.insert(itemInOccasion3);
+         itemsDAO.insert(item2InOccasion3);
+         itemsDAO.insert(item3InOccasion3);
+
+         //Expired occasion test data.
+         long id3_2 = occasionDao.insert(new OccasionModel("Mar 6, 2021", "Snacks", false, true));
+         ItemModel itemInOccasion3_2 = new ItemModel(37, "Coca Cola", 2, "Ludwig");
+         ItemModel item2InOccasion3_2 = new ItemModel(25, "Snickers Bar", 2, "Abraham");
+         ItemModel item3InOccasion3_2 = new ItemModel(30, "Twixx Bar", 3, "John");
+
+         LocationModel locationModelInOccasion3_2 = new LocationModel(59.298938,18.080310,0.0, 100.0, "Gullmarsplan T-bana, Yttre, 121 40");
+
+         itemInOccasion3_2.setOccasionID(id3_2);
+         item2InOccasion3_2.setOccasionID(id3_2);
+         item3InOccasion3_2.setOccasionID(id3_2);
+
+         locationModelInOccasion3_2.setOccasionID(id3_2);
+
+         locationDAO.insert(locationModelInOccasion3_2);
+
+         itemsDAO.insert(itemInOccasion3_2);
+         itemsDAO.insert(item2InOccasion3_2);
+         itemsDAO.insert(item3InOccasion3_2);
 
          return null;
       }
