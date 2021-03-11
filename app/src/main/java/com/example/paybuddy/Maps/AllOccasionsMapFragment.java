e86c18ba9e4bcbcbf7aadb0e1ec6106a2c7969f7
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-03-11 02:50:56 +0100

    Updated maps.

--- app/src/main/java/com/example/paybuddy/Maps/AllOccasionsMapFragment.java
+++ app/src/main/java/com/example/paybuddy/Maps/AllOccasionsMapFragment.java
@@ -5,25 +5,20 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 
 import com.example.paybuddy.Models.LocationModel;
 import com.example.paybuddy.Models.OccasionModel;
 import com.example.paybuddy.Occasions.Dialogs.DialogPreviewOccasion;
 import com.example.paybuddy.R;
-import com.example.paybuddy.Viewmodels.LocationViewModel;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -41,78 +36,72 @@ import java.util.List;
 public class AllOccasionsMapFragment extends Fragment {
     private CoordinatesViewModel coordinatesViewModel;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            coordinatesViewModel.getLocations().observe(getViewLifecycleOwner(), new Observer<List<OccasionModel>>() {
-                @Override
-                public void onChanged(List<OccasionModel> occasionModels) {
-                    googleMap.clear();
-                    if(occasionModels != null){
-                        List<LocationModel> locationModels = new ArrayList<>();
-                        for(int i = 0; i < occasionModels.size(); i++) {
-                            locationModels.add(occasionModels.get(i).getLocationModel());
-                        }
+            coordinatesViewModel.getLocations().observe(getViewLifecycleOwner(), occasionModels -> {
+                googleMap.clear();
+                if(occasionModels != null){
+                    List<LocationModel> locationModels = new ArrayList<>();
+                    for(int i = 0; i < occasionModels.size(); i++) {
+                        locationModels.add(occasionModels.get(i).getLocationModel());
+                    }
 
-                        for(int i = 0; i < occasionModels.size(); i++){
-                            OccasionModel occasionModel = occasionModels.get(i);
+                    for(int i = 0; i < occasionModels.size(); i++){
+                        OccasionModel occasionModel = occasionModels.get(i);
 
-                            if(occasionModel.getLocationModel() != null){
-                                LocationModel location = occasionModel.getLocationModel();
+                        if(occasionModel.getLocationModel() != null){
+                            LocationModel location = occasionModel.getLocationModel();
 
-                                LatLng occasionLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            LatLng occasionLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-                                BitmapDescriptor warningIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_warning_24);
-                                BitmapDescriptor pendingIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_pending_actions_24);
-                                BitmapDescriptor historyIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_history_24);
+                            BitmapDescriptor warningIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_warning_24);
+                            BitmapDescriptor pendingIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_pending_actions_24);
+                            BitmapDescriptor historyIcon = bitmapDescriptorFromVector(getActivity(), R.drawable.ic_baseline_history_24);
 
-                                if(occasionModel.isExpired()){
-                                    Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
-                                            .position(occasionLocation)
-                                            .title(occasionModels.get(i).getDescription())
-                                            .snippet("This occasion expire " + occasionModel.getDate() + " and has "
-                                                    + occasionModel.getItems().size() + " items. It's marked as: Expired!")
-                                            .icon(warningIcon));
+                            if(occasionModel.isExpired()){
+                                Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
+                                        .position(occasionLocation)
+                                        .title(occasionModels.get(i).getDescription())
+                                        .snippet("This occasion expire " + occasionModel.getDate() + " and has "
+                                                + occasionModel.getItems().size() + " items. It's marked as: Expired!")
+                                        .icon(warningIcon));
 
-                                    myMapMarker.setTag(occasionModel);
-                                }
-
-                                if(occasionModel.isPaid()){
-                                    Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
-                                            .position(occasionLocation)
-                                            .title(occasionModels.get(i).getDescription())
-                                            .snippet("This occasion expire " + occasionModel.getDate() + " and has "
-                                                    + occasionModel.getItems().size() + " items. It's marked as: Paid!")
-                                            .icon(historyIcon));
-
-                                    myMapMarker.setTag(occasionModel);
-                                }
-
-                                if(!occasionModel.isPaid() && !occasionModel.isExpired()){
-                                    Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
-                                            .position(occasionLocation)
-                                            .title(occasionModels.get(i).getDescription())
-                                            .snippet("This occasion expire " + occasionModel.getDate() + " and has "
-                                                    + occasionModel.getItems().size() + " items. It's marked as: Paid!")
-                                            .icon(pendingIcon));
-
-                                    myMapMarker.setTag(occasionModel);
-                                }
-
-
-                                CameraUpdate centroidLocation = CameraUpdateFactory.newLatLngZoom(GetCentralGeoCoordinate(locationModels), 10);
-                                googleMap.animateCamera(centroidLocation);
-
-                                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                                    @Override
-                                    public boolean onMarkerClick(Marker marker) {
-                                        OccasionModel markerObject = (OccasionModel) marker.getTag();
-                                        DialogPreviewOccasion dialogFragment = new DialogPreviewOccasion(markerObject);
-                                        dialogFragment.show(getChildFragmentManager(), "Test");
-                                        return false;
-                                    }
-                                });
+                                myMapMarker.setTag(occasionModel);
                             }
+
+                            if(occasionModel.isPaid()){
+                                Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
+                                        .position(occasionLocation)
+                                        .title(occasionModels.get(i).getDescription())
+                                        .snippet("This occasion expire " + occasionModel.getDate() + " and has "
+                                                + occasionModel.getItems().size() + " items. It's marked as: Paid!")
+                                        .icon(historyIcon));
+
+                                myMapMarker.setTag(occasionModel);
+                            }
+
+                            if(!occasionModel.isPaid() && !occasionModel.isExpired()){
+                                Marker myMapMarker = googleMap.addMarker(new MarkerOptions()
+                                        .position(occasionLocation)
+                                        .title(occasionModels.get(i).getDescription())
+                                        .snippet("This occasion expire " + occasionModel.getDate() + " and has "
+                                                + occasionModel.getItems().size() + " items. It's marked as: Paid!")
+                                        .icon(pendingIcon));
+
+                                myMapMarker.setTag(occasionModel);
+                            }
+
+
+                            CameraUpdate centroidLocation = CameraUpdateFactory.newLatLngZoom(GetCentralGeoCoordinate(locationModels), 10);
+                            googleMap.animateCamera(centroidLocation);
+
+                            googleMap.setOnMarkerClickListener(marker -> {
+                                OccasionModel markerObject = (OccasionModel) marker.getTag();
+                                DialogPreviewOccasion dialogFragment = new DialogPreviewOccasion(markerObject);
+                                dialogFragment.show(getChildFragmentManager(), "Test");
+                                return false;
+                            });
                         }
                     }
                 }
@@ -123,7 +112,7 @@ public class AllOccasionsMapFragment extends Fragment {
     /**
      * Calculate center point between 1...N number of coordinates.
      * @param geoCoordinates List of locations (latitude, longitude).
-     * @return
+     * @return LatLng
      */
     public static LatLng GetCentralGeoCoordinate(List<LocationModel> geoCoordinates)
     {
@@ -160,6 +149,7 @@ public class AllOccasionsMapFragment extends Fragment {
     }
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
         Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        assert vectorDrawable != null;
         vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
         Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
@@ -173,7 +163,7 @@ public class AllOccasionsMapFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View mapsView = inflater.inflate(R.layout.fragment_maps, container, false);
-        coordinatesViewModel = new ViewModelProvider(getActivity()).get(CoordinatesViewModel.class);
+        coordinatesViewModel = new ViewModelProvider(requireActivity()).get(CoordinatesViewModel.class);
         return mapsView;
     }
 
--- app/src/main/java/com/example/paybuddy/Maps/MapsPointFragment.java
+++ app/src/main/java/com/example/paybuddy/Maps/MapsPointFragment.java
@@ -1,61 +1,38 @@
 package com.example.paybuddy.Maps;
 
-import androidx.annotation.DrawableRes;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.paybuddy.Models.LocationModel;
-import com.example.paybuddy.Models.OccasionModel;
-import com.example.paybuddy.Models.OccasionWithItems;
 import com.example.paybuddy.R;
-import com.example.paybuddy.Viewmodels.OccasionViewModel;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapsPointFragment extends Fragment {
     private CoordinatesViewModel coordinatesViewModel;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            coordinatesViewModel.getLocation().observe(getViewLifecycleOwner(), new Observer<LocationModel>() {
-                @Override
-                public void onChanged(LocationModel locationModel) {
-                    googleMap.clear();
-                    if(locationModel != null){
-                        LatLng occasionLocation = new LatLng(locationModel.getLatitude(), locationModel.getLongitude());
-                        googleMap.addMarker(new MarkerOptions().position(occasionLocation).title("Marker for occasion location."));
-                        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(occasionLocation, 15);
-                        googleMap.animateCamera(yourLocation);
-                    }
+            coordinatesViewModel.getLocation().observe(getViewLifecycleOwner(), locationModel -> {
+                googleMap.clear();
+                if(locationModel != null){
+                    LatLng occasionLocation = new LatLng(locationModel.getLatitude(), locationModel.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(occasionLocation).title("Marker for occasion location."));
+                    CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(occasionLocation, 15);
+                    googleMap.animateCamera(yourLocation);
                 }
             });
         }
@@ -67,7 +44,7 @@ public class MapsPointFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View mapsView = inflater.inflate(R.layout.fragment_maps, container, false);
-        coordinatesViewModel = new ViewModelProvider(getActivity()).get(CoordinatesViewModel.class);
+        coordinatesViewModel = new ViewModelProvider(requireActivity()).get(CoordinatesViewModel.class);
         return mapsView;
     }
 
